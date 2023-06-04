10bb3806129b2923b7e67e078bfc254d2a368d5f
skolanko222 <skolanko@student.agh.edu.pl>
2023-06-04 20:08:15 +0200

    workflow bugfix

--- src/include/GUI.h
+++ src/include/GUI.h
@@ -48,31 +48,10 @@ class MainFrame : public wxFrame
 		wxFilePickerCtrl* m_filePicker2;
 
 
-
-		// Virtual event handlers, override them in your derived class
-		virtual void m_panel0OnPaint( wxPaintEvent& event ) { event.Skip(); } // main panel
-		virtual void m_panel0OnUpdateUI( wxUpdateUIEvent& event ) { event.Skip(); }
-		virtual void m_panel1OnPaint( wxPaintEvent& event ) { event.Skip(); }
-		virtual void m_panel1OnUpdateUI( wxUpdateUIEvent& event ) { event.Skip(); }
-		virtual void m_panel2OnPaint( wxPaintEvent& event ) { event.Skip(); }
-		virtual void m_panel2OnUpdateUI( wxUpdateUIEvent& event ) { event.Skip(); }
-		virtual void m_panel3OnPaint( wxPaintEvent& event ) { event.Skip(); }
-		virtual void m_panel3OnUpdateUI( wxUpdateUIEvent& event ) { event.Skip(); }
-		virtual void m_panel4OnPaint( wxPaintEvent& event ) { event.Skip(); }
-		virtual void m_panel4OnUpdateUI( wxUpdateUIEvent& event ) { event.Skip(); }
-		virtual void m_panel5OnPaint( wxPaintEvent& event ) { event.Skip(); }
-		virtual void m_panel5OnUpdateUI( wxUpdateUIEvent& event ) { event.Skip(); }
-		virtual void m_button2OnButtonClick( wxCommandEvent& event ) { event.Skip(); }
-		virtual void m_button3OnButtonClick( wxCommandEvent& event ) { event.Skip(); }
-		virtual void m_slider1OnScroll( wxScrollEvent& event ) { event.Skip(); }
-		virtual void m_slider1OnSlider( wxCommandEvent& event ) { event.Skip(); }
-		virtual void m_filePicker2OnFileChanged( wxFileDirPickerEvent& event ) { event.Skip(); }
-
-
 	public:
 
 		MainFrame( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxT("RASTER GRAPHICS INTERPOLATION"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 623,345 ), long style = wxDEFAULT_FRAME_STYLE|wxTAB_TRAVERSAL );
-
+		~MainFrame();
 
 		// Virtual event handlers, override them in your derived class
 		virtual void m_panel0OnPaint( wxPaintEvent& event ) { event.Skip(); } // main panel
@@ -95,12 +74,6 @@ class MainFrame : public wxFrame
 
 		// prevents flashing panel0
 		void erase_bacground(wxEraseEvent& event) {};
-
-
-	public:
-
-		MainFrame( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxT("RASTER GRAPHICS INTERPOLATION"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 623,345 ), long style = wxDEFAULT_FRAME_STYLE|wxTAB_TRAVERSAL );
-		~MainFrame();
 
 };
 
